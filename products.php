aeeb876d49ec63648888a2b05899705fda72ca47
Sean Thorpe <sean.thorpe@west-mec.org>
2016-02-01 14:14:31 -0700

    First edition of website

---
+++ products.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="homepage.css"/>
+</head>
+<body>
+<ul>
+    <li><a href="homepage.php">Home</a></li>
+    <li><a class="active" href="products.php">Products</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
+<div style="margin-left:15%;padding:1px 16px;height:1000px;">
+    <div id="page">
+        <div id="navbar">
+
+        </div>
+        <div id="body">
+            <table id="productTable">
+                <tr id="productRow1" class="productRow">
+                    <td id="product1" class="product">
+                        <img src="http://content.ibuypower.com/Images/Components/8811/VersaH35_02-kb.png" style="height: 350px;width: 350px;"/>
+                        <p></p>
+                    </td>
+                    <td id="product2" class="product">
+                        <img src="http://content.ibuypower.com/Images/Components/8592/Chimera5_White-1-Main-kb.png" style="height: 350px;width: 350px;"/>
+                    </td>
+                    <td id="product3" class="product">
+                        <img src="http://content.ibuypower.com/Images/Components/9226/NinjaII_01_600-n.png" style="height: 350px; width: 350px;"/>
+                        <p class="computer_whole">Super Bowl Intel Z170 I7</p>
+                    </td>
+                </tr>
+                <tr id="productRow2" class="productRow">
+                    <td id="product4" class="product">Put pic here</td>
+                    <td id="product5" class="product">Put pic here</td>
+                    <td id="product6" class="product">Put pic here</td>
+                </tr>
+                <tr id="productRow3" class="productRow">
+                    <td id="product7" class="product">Put pic here</td>
+                    <td id="product8" class="product">Put pic here</td>
+                    <td id="product9" class="product">Put pic here</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
